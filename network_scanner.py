ab6caea32a157b8eabcca02cec219297e536cb2f
nbalazs1337 <negrea.balazs@yahoo.com>
2022-02-18 15:53:02 -0500

    Printing all the responses from the arp_request and storing them in lists

--- network_scanner.py
+++ network_scanner.py
@@ -3,21 +3,14 @@
 import scapy.all as scapy
 
 def scan(ip):
-    #creating an ARP packet
-    arp_request = scapy.ARP(pdst=ip)
-    #for testing purpose, we can make use of the command
-    #print(arp_request.summary())
-    # listing all the fields for ARP function
-    #scapy.ls(scapy.ARP())
-    #we are creating an ethernet frame, and ethernet object from a class implemented by scapy
-    broadcast = scapy.Ether()
-    #creating a new packet which is the combination of the other two packets
-    #we use forward slash becouse ware allowed by the scapy library
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    # for showing more details about the contents of the packet
-    arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1)[0]
 
+    for element in answered_list:
+        print(element)
+        print("-------------------")
 
 
 
