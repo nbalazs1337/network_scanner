c7291d2576fe09c3cc50cae6a61b2a19fc69200c
nbalazs1337 <negrea.balazs@yahoo.com>
2022-02-19 15:16:45 -0500

    Storing the responses into a list of dictionaries, finishing the project

--- network_scanner.py
+++ network_scanner.py
@@ -6,12 +6,21 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
 
+    client_list = []
     for element in answered_list:
-        print(element)
-        print("-------------------")
+        client_dict = {"ip":element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+
+    return client_list
+
+def print_result(result_list):
+    print("IP\t\t\tMAC Adress\n----------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
 
 
-scan("10.0.2.1/24")
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
